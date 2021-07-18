28099bd5011de6981a7c5412783952cc7601ae0c
youyifeng <842056007@qq.com>
2021-07-18 22:21:03 +0800

    modify fkmodule.py

--- 官方配套代码/16/16.1/fkmodule.py
+++ 官方配套代码/16/16.1/fkmodule.py
@@ -15,6 +15,7 @@
 #########################################################################
 MY_NAME = '疯狂软件教育中心'
 
+
 def say_hi(name):
     '''
     定义一个打招呼的函数
@@ -22,6 +23,8 @@ def say_hi(name):
     '''
     print("执行say_hi函数")
     return name + '您好！'
+
+
 def print_rect(height, width):
     '''
     定义一个打印矩形的函数
@@ -29,12 +32,15 @@ def print_rect(height, width):
     width - 代表矩形的宽
     '''
     print(('*' * width + '\n') * height)
+
+
 class User:
     NATIONAL = 'China'
     '''
     定义一个代表用户的类
     该类包括name、age两个变量
     '''
+
     def __init__(self, name, age):
         '''
         name初始化该用户的name
@@ -42,10 +48,10 @@ class User:
         '''
         self.name = name
         self.age = age
-    def eat (food):
+
+    def eat(self, food):
         '''
         定义用户吃东西的方法
         food - 代表用户正在吃的东西
         '''
         print('%s正在吃%s' % (self.name, food))
-
